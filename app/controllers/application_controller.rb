5ae66511173935fe352944344aa8a2c619129b1b
dave <dave.buenconsejo@gmail.com>
2015-01-24 07:04:45 +0000

    fixed API without disabling CSRF protection

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,21 +1,20 @@
 class ApplicationController < ActionController::Base
+  include DeviseTokenAuth::Concerns::SetUserByToken
+  include ActionController::MimeResponds
   # Prevent CSRF attacks by raising an exception.
   # For APIs, you may want to use :null_session instead.
   #protect_from_forgery with: :exception
   skip_before_filter :protect_from_forgery
-	before_filter :allow_cors
+  after_filter :set_csrf_cookie
 
-	def allow_cors
-	origin = request.headers["Origin"]
-	headers['Access-Control-Allow-Origin'] = origin
-	headers['Access-Control-Allow-Methods'] = 'POST, GET, OPTIONS, PUT, DELETE'
-	allow_headers = request.headers["Access-Control-Request-Headers"]
-	if allow_headers.nil?
-		allow_headers = 'Origin, Authorization, Accept, Content-Type'
-	end
+  def set_csrf_cookie
+    if protect_against_forgery?
+      cookies['XSRF-TOKEN'] = form_authenticity_token
+    end
+  end
+  protected
 
-	headers['Access-Control-Allow-Headers'] = allow_headers
-	headers['Access-Control-Allow-Credentials'] = 'true'
-	headers['Access-Control-Max-Age'] = '1728000'
-	end
+  def verified_request?
+    super || valid_authenticity_token?(session, request.headers['X-XSRF-TOKEN'])
+  end
 end
--- app/models/user.rb
+++ app/models/user.rb
@@ -1,7 +1,10 @@
 class User < ActiveRecord::Base
+  include DeviseTokenAuth::Concerns::User
   # Include default devise modules. Others available are:
   # :confirmable, :lockable, :timeoutable and :omniauthable
   devise :database_authenticatable, :registerable,
          :recoverable, :rememberable, :trackable,
-         :validatable, :omniauthable
+         :validatable , :omniauthable
+  before_save -> { skip_confirmation! }
+  attr_accessor :username, :confirm_success_url, :config_name, :registration
 end
