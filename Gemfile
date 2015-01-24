d1946e987fd5c755f6908de78f83fd35d49ad887
dave <dave.buenconsejo@gmail.com>
2015-01-24 06:04:38 +0000

    added omniauth

--- Gemfile
+++ Gemfile
@@ -3,6 +3,7 @@ source 'https://rubygems.org'
 gem 'rack-cors'
 gem 'devise', '~> 3.4.0'
 gem 'devise_token_auth', git: 'https://github.com/jasonswett/devise_token_auth.git'
+gem 'omniauth', '>= 1.0.0'
 # Bundle edge Rails instead: gem 'rails', github: 'rails/rails'
 gem 'rails', '4.2.0'
 # Use mysql as the database for Active Record
--- config/routes.rb
+++ config/routes.rb
@@ -1,9 +1,9 @@
 Rails.application.routes.draw do
   devise_for :users
   resources :posts
-	scope '/api' do 
-		mount_devise_token_auth_for :users, at: '/auth'
-	end
+  scope '/api' do
+    mount_devise_token_auth_for 'User', at: '/auth'
+  end
 
 
   # The priority is based upon order of creation: first created -> highest priority.
